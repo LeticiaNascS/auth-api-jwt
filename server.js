9ae2adf641bf4b98f75105edf73b4b78bc368558
Leticia Nascimento <let.lu.nascimento@gmail.com>
2026-04-15 13:16:09 -0300

    CONSOLE LOG

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import app from "./app.js";
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
+  console.log(`VARIAVEL DE AMBIENTE ${process.env.JWT_SECRET}`);
   console.log(`Servidor está rodando na porta ${port}`);
 });
 
