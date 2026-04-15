de3022fcf32e1682fe820c55b599aaf1f8338e92
Leticia Nascimento <let.lu.nascimento@gmail.com>
2026-04-15 13:18:04 -0300

    REMOVIDO LOGS DESNECESSARIOS

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import app from "./app.js";
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`VARIAVEL DE AMBIENTE ${process.env.JWT_SECRET}`);
   console.log(`Servidor está rodando na porta ${port}`);
 });
 
